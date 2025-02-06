e860abb3bb037f3bfc4afed965181235a7bf8564
Hassan Momin <hassanwm99@gmail.com>
2025-02-06 20:44:54 +0530

    variables completed

--- variables.dart
+++ variables.dart
@@ -1,7 +1,12 @@
 void main() {
   // <datatype> variableName = value;
-  int num1 = 20;
+  int num1 = -20;
   int num2 = 30;
+
+  dynamic someValue = "Hassan";
+  print(someValue);
+  print(someValue.runtimeType);
+  print(num2.abs());
   var addition = num1 + num2;
   var subtraction = num1 - num2;
   var multiplication = num1 * num2;
