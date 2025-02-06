56b5a3ecde28e1359c3ca599c53587fe5c78da28
Hassan Momin <hassanwm99@gmail.com>
2025-02-06 19:40:55 +0530

    first commit

---
+++ intro.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Welcome To Dart!");
+  print(10);
+  print(3 < 9);
+  //dart uses Bodmas
+  print(3 + 2 - 2 * 2 / 4);
+  print("2" + "1");
+  print("2" * 3);
+}
---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  // <datatype> variableName = value;
+  int num1 = 20;
+  int num2 = 30;
+  var addition = num1 + num2;
+  var subtraction = num1 - num2;
+  var multiplication = num1 * num2;
+  var division = num1 / num2;
+  print(addition);
+  print(subtraction);
+  print(multiplication);
+  print(division);
+}
